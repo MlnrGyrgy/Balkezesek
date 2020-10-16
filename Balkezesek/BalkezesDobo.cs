e6dbef52c10e7fdee6a93cf3d3b361195faccd4f
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-10-16 11:35:46 +0200

    BalkezesDobo osztaly letrehozasa es az adatok beolvasasa

---
+++ Balkezesek/BalkezesDobo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class BalkezesDobo
+    {
+        public string nev { get; private set; }
+        public string elso { get; private set; }
+        public string utolso { get; private set; }
+        public int suly { get; private set; }
+        public int magassag { get; private set; }
+
+        public BalkezesDobo(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elso = elso;
+            this.utolso = utolso;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+    }
+}
--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Balkezesek
 {
     class Program
     {
+        static List<BalkezesDobo> balos = new List<BalkezesDobo>();
+        static void Beolvasas()
+        {
+            StreamReader file = new StreamReader("balkezesek.csv");
+            file.ReadLine();
+            while (!file.EndOfStream)
+            {
+                string[] egy = file.ReadLine().Split(';');
+                balos.Add(new BalkezesDobo(egy[0],egy[1],egy[2], int.Parse(egy[3]), int.Parse(egy[4])));
+            }
+            file.Close();
+        }
         static void Main(string[] args)
         {
+            Beolvasas();
+           
+            Console.ReadKey();
         }
     }
 }
